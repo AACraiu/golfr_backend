998b80a2a647dd376ef00484c566263182b688af
Ioana Sechel <ioana.sechel@golfgenius.com>
2021-12-22 14:37:10 +0200

    Improve feed performance

--- app/controllers/api/scores_controller.rb
+++ app/controllers/api/scores_controller.rb
@@ -5,7 +5,7 @@ module Api
     before_action :validate_score_user_id, only: :destroy
 
     def user_feed
-      scores = Score.all.order(played_at: :desc, id: :desc)
+      scores = Score.all.order(played_at: :desc, id: :desc).includes(:user)
       serialized_scores = scores.map(&:serialize)
 
       response = {
