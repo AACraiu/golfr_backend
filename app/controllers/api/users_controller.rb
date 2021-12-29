a03c8aa8fcc5aea5fe34f26963ee144817c0b2ea
Papa-Cristian <cristian.pap@golfgenius.com>
2021-12-29 15:24:23 +0200

    Add user and user scores pages

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,6 +2,8 @@ module Api
   # Controller that handles authorization and user data fetching
   class UsersController < ApplicationController
     include Devise::Controllers::Helpers
+    before_action :logged_in!, only: [:show, :scores]
+    before_action :validate_user_id, only: [:show, :scores]
 
     def login
       user = User.find_by('lower(email) = ?', params[:email])
@@ -25,6 +27,43 @@ module Api
           token: current_token
         }
       }.to_json
+    end
+
+    def show
+      response = {
+        user: {
+          id: @user.id,
+          name: @user.name
+        },
+      }
+
+      render json: response.to_json
+    end
+
+    def scores
+      scores = @user.scores.order(played_at: :desc)
+
+      serialized_scores = scores.map(&:serialize)
+
+      response = {
+        scores: serialized_scores,
+      }
+
+      render json: response.to_json
+    end
+
+    private
+
+    def validate_user_id
+      @user = User.find_by(id: params[:id])
+
+      return if !@user.nil?
+
+      render json: {
+        errors: [
+          'The given user id was not found'
+        ]
+      }, status: :not_found
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   namespace :api do
     post 'login', to: 'users#login'
     get 'feed', to: 'scores#user_feed'
+    get 'user/:id', to: 'users#show'
+    get 'user/:id/scores', to: 'users#scores'
     resources :scores, only: %i[create destroy]
   end
 end
