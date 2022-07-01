49d2ed11e352e8179104a7d455a644e5cbde8844
Melisa <eu_melisa_andra@yahoo.com>
2022-07-01 13:50:06 +0300

    Added more seeds to the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,35 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(
-  name: 'George Marcus',
-  email: 'gmarcus@golfr.com',
-  password: '123456',
-  password_confirmation: '123456'
-)
-
-User.create!(
-  name: 'Sergiu Apostu',
-  email: 'sapostu@golfr.com',
-  password: '123456',
-  password_confirmation: '123456'
-)
-
-User.create!(
-  name: 'Alex Tandrau',
-  email: 'atandrau@golfr.com',
-  password: '123456',
-  password_confirmation: '123456'
-)
 
 rng = Random.new
 now = Time.zone.today
 User.all.each do |user|
-  5.times do |i|
+  10.times do |i|
     Score.create!(
       user: user,
       total_score: rng.rand(66..99),
-      played_at: now - 5.days + i.days
+      played_at: now - 20.days + i.days
     )
   end
 end
