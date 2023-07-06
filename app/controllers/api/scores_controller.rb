02a73bc8caccdbb0e6c95556a7d5c291d34c5e7f
Hartan Silviu <hartansilviu@192.168.1.10>
2023-07-06 13:04:22 +0300

    Minor fix

--- app/controllers/api/scores_controller.rb
+++ app/controllers/api/scores_controller.rb
@@ -30,6 +30,28 @@ module Api
       render json: response.to_json
     end
 
+    def create
+      score = current_user.scores.build(score_params)
+
+      if score.save
+        render json: {
+          score: score.serialize
+        }
+      else
+        render json: {
+          errors: score.errors.messages
+        }, status: :bad_request
+      end
+    end
+
+    def destroy
+      @score.destroy!
+
+      render json: {
+        score: @score.serialize
+      }
+    end
+
     private
 
     def score_params
